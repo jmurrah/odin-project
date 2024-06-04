0aa437196aee7ab443419faeb6910136c65153a9
jmurrah <jacobhmurrah@gmail.com>
2024-06-04 07:13:03 -0500

    Complete Etch-a-Sketch project

--- foundations/javascript/etch-a-sketch/sketch.js
+++ foundations/javascript/etch-a-sketch/sketch.js
@@ -1,21 +1,46 @@
 const WIDTH = 800;
-const container = document.querySelector('#container');
-container.style = `display: flex; flex-wrap: wrap; width: ${WIDTH}px; height: ${WIDTH}px;`;
+const grid = document.querySelector('#grid');
 
-window.onload = () => {
-    createGrid(16);
+grid.style = `display: flex; flex-wrap: wrap; width: ${WIDTH}px; height: ${WIDTH}px;`;
+
+
+function deleteGrid() {
+    while (grid.lastChild) {
+        grid.removeChild(grid.lastChild);
+    }
 }
 
 function createGrid(size) {
-    console.log(size);
+    deleteGrid();
     for (let i = 0; i < size ** 2; i++) {
         let div = document.createElement('div');
         div.style = `width: ${WIDTH / size}px; height: ${WIDTH / size}px; background-color: white; border: 1px solid black;`;
-        container.appendChild(div);
+        grid.appendChild(div);
     }
 }
 
 document.querySelector('#resize').addEventListener('click', () => {
-    input = document.querySelector('#input').value;
-    createGrid(input);
+    const size = document.querySelector('#input').value;
+    if (size < 1 || size > 100) {
+        alert('Please enter a number between 1 and 100');
+        return;
+    } else {
+        createGrid(size);
+    }
 });
+
+document.querySelector('#clear').addEventListener('click', () => {
+    grid.childNodes.forEach((div) => {
+        div.style.backgroundColor = 'white';
+    });
+});
+
+grid.addEventListener('mouseover', (e) => {
+    if (e.target.style.backgroundColor === 'white') {
+        e.target.style.backgroundColor = 'black';
+    }
+});
+
+window.onload = () => {
+    createGrid(16);
+}
