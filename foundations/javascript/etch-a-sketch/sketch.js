945ddef73a5d860a44d8a80fae41fbf89fce2f10
jmurrah <jacobhmurrah@gmail.com>
2024-06-03 21:46:49 -0500

    Need to get old div to delete when new div being created

--- foundations/javascript/etch-a-sketch/sketch.js
+++ foundations/javascript/etch-a-sketch/sketch.js
@@ -1,11 +1,21 @@
 const WIDTH = 800;
 const container = document.querySelector('#container');
-
 container.style = `display: flex; flex-wrap: wrap; width: ${WIDTH}px; height: ${WIDTH}px;`;
-container.style.backgroundColor = 'black';
 
-for (let i = 0; i < 50 ** 2; i++) {
-    let div = document.createElement('div');
-    div.style = `width: ${WIDTH / 50}px; height: ${WIDTH / 50}px; background-color: white; border: 1px solid black;`;
-    container.appendChild(div);
+window.onload = () => {
+    createGrid(16);
 }
+
+function createGrid(size) {
+    console.log(size);
+    for (let i = 0; i < size ** 2; i++) {
+        let div = document.createElement('div');
+        div.style = `width: ${WIDTH / size}px; height: ${WIDTH / size}px; background-color: white; border: 1px solid black;`;
+        container.appendChild(div);
+    }
+}
+
+document.querySelector('#resize').addEventListener('click', () => {
+    input = document.querySelector('#input').value;
+    createGrid(input);
+});
