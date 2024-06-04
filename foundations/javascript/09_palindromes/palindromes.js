0da2c157c0a1307f1c43db13be5daaba12619a6e
jmurrah <jacobhmurrah@gmail.com>
2024-06-04 15:36:49 -0500

    Complete js exercises 8-10

--- foundations/javascript/09_palindromes/palindromes.js
+++ foundations/javascript/09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const formattedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return formattedStr === formattedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- foundations/javascript/10_fibonacci/fibonacci.js
+++ foundations/javascript/10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n !== 'number') {
+        n = parseInt(n)
+    } else if (n < 0) {
+        return "OOPS";
+    } 
 
+    if (n === 0) {
+        return 0;
+    }
+
+    let first = 1;
+    let second = 0;
+    for (let i = 2; i <= n; i++) {
+        let current = first + second;
+        second = first;
+        first = current;
+    }
+
+    return first;
 };
 
 // Do not edit below this line
