ca88f40b9866f358c572f71b10a979d051f5f711
jmurrah <jacobhmurrah@gmail.com>
2024-06-04 20:58:15 -0500

    Complete js exercises 11-12

--- foundations/javascript/12_findTheOldest/findTheOldest.js
+++ foundations/javascript/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        const age = ("yearOfDeath" in person) ? person["yearOfDeath"] - person["yearOfBirth"] : new Date().getFullYear() - person["yearOfBirth"];
+        return age > oldestPerson["age"] ? {"name": person["name"], "age": age} : oldestPerson;
+    }, {"name": "", "age": 0});
 };
 
 // Do not edit below this line
